45febdef0011ff8b6415d7a457f95cfdd53229bf
bhaskarkrp <bhaskarkumar0022@gmail.com>
2024-10-11 18:13:44 +0530

    TDD feature

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,24 +1,27 @@
 function add(numbers) {
-    if (!numbers) return 0;
+  if (!numbers) return 0;
 
-    // Custom delimiter handling
-    let delimiter = /,|\n/;
-    if (numbers.startsWith("//")) {
-        const delimiterSpec = numbers.match(/\/\/(.+)\n/);
-        delimiter = new RegExp(delimiterSpec[1]);
-        numbers = numbers.replace(/\/\/.+\n/, "");
-    }
+  // Custom delimiter handling
+  let delimiter = /,|\n/;
+  if (numbers.startsWith("//")) {
+    const delimiterSpec = numbers.match(/\/\/(.+)\n/);
+    delimiter = new RegExp(delimiterSpec[1]);
+    numbers = numbers.replace(/\/\/.+\n/, "");
+  }
 
-    // Split numbers by delimiters
-    const nums = numbers.split(delimiter).map(Number);
+  // Split numbers by delimiters
+  const nums = numbers
+    .split(delimiter)
+    .map(Number)
+    .filter((num) => num <= 1000);
 
-    // Check for negative numbers
-    const negatives = nums.filter(n => n < 0);
-    if (negatives.length) {
-        throw new Error(`negative numbers not allowed: ${negatives.join(",")}`);
-    }
+  // Check for negative numbers
+  const negatives = nums.filter((n) => n < 0);
+  if (negatives.length) {
+    throw new Error(`negative numbers not allowed: ${negatives.join(",")}`);
+  }
 
-    return nums.reduce((sum, num) => sum + num, 0);
+  return nums.reduce((sum, num) => sum + num, 0);
 }
 
 module.exports = { add };
